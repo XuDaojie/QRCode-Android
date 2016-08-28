9cf0aa2d270e12d9fc04b9a7654c519f1af7bbaf
XuDaojie <xudaojieg@gmail.com>
2016-08-28 23:41:26 +0800

    可识别中文二维码图片

--- app/src/main/java/io/github/xudaojie/qrcode/MainActivity.java
+++ app/src/main/java/io/github/xudaojie/qrcode/MainActivity.java
@@ -1,11 +1,31 @@
 package io.github.xudaojie.qrcode;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.DecodeHintType;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.common.HybridBinarizer;
+
+import java.util.HashMap;
+
+import io.github.xudaojie.qrcode.common.ActionUtils;
+import io.github.xudaojie.qrcode.common.QrUtils;
+import io.github.xudaojie.qrcode.zxing.camera.PlanarYUVLuminanceSource;
+
+import static io.github.xudaojie.qrcode.common.QrUtils.getYUV420sp;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,24 +35,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button qRCodeBtn = (Button) findViewById(R.id.qrcode_btn);
-        qRCodeBtn.setOnClickListener(new View.OnClickListener() {
+        final Button qrCodeBtn = (Button) findViewById(R.id.qrcode_btn);
+        Button qrCodeReadBtn = (Button) findViewById(R.id.qrcode_read_btn);
+        qrCodeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this, CaptureActivity.class);
                 MainActivity.this.startActivityForResult(i, REQUEST_QR_CODE);
             }
         });
+        qrCodeReadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActionUtils.startActivityForGallery(MainActivity.this);
+            }
+        });
+
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_QR_CODE
-                && resultCode == RESULT_OK
+        if (resultCode == RESULT_OK
+                && requestCode == REQUEST_QR_CODE
                 && data != null) {
             String result = data.getStringExtra("result");
             Toast.makeText(MainActivity.this, result, Toast.LENGTH_SHORT).show();
+        } else if (resultCode == RESULT_OK
+                && requestCode == ActionUtils.PHOTO_REQUEST_GALLERY
+                && data != null) {
+            Uri inputUri = data.getData();
+
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(inputUri, proj, null, null, null);
+            if (cursor.moveToFirst()) {
+                String path = cursor.getString(cursor.getColumnIndex(MediaStore.MediaColumns.DATA));
+                decode(path);
+            }
         }
     }
+
+    private void decode(final String path) {
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+
+                Bitmap bitmap = QrUtils.decodeSampledBitmapFromFile(path, 256, 256);
+
+//                Bitmap bitmap = getDecodeAbleBitmap(R.drawable.qrcode_zh);
+                int width = bitmap.getWidth();
+                int height = bitmap.getHeight();
+                int[] pixels = new int[width * height];
+                bitmap.getPixels(pixels, 0, width, 0, 0, width, height);
+//                RGBLuminanceSource source = new RGBLuminanceSource(width, height, pixels);
+                PlanarYUVLuminanceSource source1 = new PlanarYUVLuminanceSource(getYUV420sp(width, height, bitmap), width, height, 0, 0, width, height);
+                BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source1));
+                HashMap<DecodeHintType, Object> hints = new HashMap<>();
+
+                hints.put(DecodeHintType.TRY_HARDER, Boolean.TRUE);
+                hints.put(DecodeHintType.PURE_BARCODE, Boolean.FALSE);
+                hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+
+                try {
+                    Result result = new MultiFormatReader().decode(binaryBitmap, hints);
+                    Log.d("MainActivity", result.getText());
+                } catch (NotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
 }
